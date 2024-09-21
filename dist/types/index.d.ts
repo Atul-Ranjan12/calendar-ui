ec3329e46314dfdd47daa948054d037d8a91732d
Atul-Ranjan12 <atulranjan975@gmail.com>
2024-09-21 17:13:38 +0530

    Updated package

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -3,3 +3,5 @@ export { BookingCalendar } from "./stories/BookingCalendar";
 export { Calendar } from "./stories/Calendar";
 export { SimpleCalendar } from "./stories/SimpleCalendar";
 export { SimpleRangePicker } from "./stories/SimpleRangePicker";
+export { DatePicker } from "./stories/DatePicker";
+export { RangeDatePicker } from "./stories/RangeDatePicker";
